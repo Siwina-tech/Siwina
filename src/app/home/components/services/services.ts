6f2311b79736d099b40e511eea9883793b420dac
LuiferEduardoo <luifer01ortegaperez@gmail.com>
2026-03-31 21:34:43 -0500

    feat: Add services

--- src/app/home/components/services/services.ts
+++ src/app/home/components/services/services.ts
@@ -29,16 +29,34 @@ export class Services {
         "ERPs, CRMs y soluciones a medida que optimizan los procesos de tu empresa.",
     },
     {
-      icon: "cloud",
-      title: "Cloud & DevOps",
-      description:
-        "Infraestructura en la nube, CI/CD y automatización para máxima eficiencia.",
-    },
-    {
       icon: "autorenew",
       title: "Automatización de procesos",
       description:
         "Automatizaciones inteligentes para optimizar y escalar tu operación.",
+    },
+    {
+      icon: "chat",
+      title: "Chat Bots & agentes inteligentes",
+      description:
+        "Soluciones de atención al cliente automatizadas con inteligencia artificial.",
+    },
+    {
+      title: "Llamadas con IA",
+      icon: "call",
+      description:
+        "Sistemas de llamadas automatizadas con IA para mejorar la comunicación con tus clientes.",
+    },
+    {
+      title: "Análisis predictivo y detección de fraudes",
+      icon: "analytics",
+      description:
+        "Modelos de machine learning para predecir tendencias y detectar actividades fraudulentas.",
+    },
+    {
+      icon: "cloud",
+      title: "Cloud & DevOps",
+      description:
+        "Infraestructura en la nube, CI/CD y automatización para máxima eficiencia.",
     },
     {
       icon: "show_chart",
